b705f6aac6d459b7c02e26aff8d9ab174d103ad1
CodeWithYash99 <yashwanthrangu99@gmail.com>
2024-05-17 13:23:25 +0530

    Add Contact Component

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact-bg-container flex flex-col justify-center" id="contact">
+      <h1 className="about-heading">CONTACT ME</h1>
+
+      <p className="text-center">
+        Far far away, behind the word mountains, far from the countries.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
