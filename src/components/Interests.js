2e14db82cd0985a40bee88d642cfee767634f940
CodeWithYash99 <yashwanthrangu99@gmail.com>
2024-05-17 13:23:07 +0530

    Add Interests Component

---
+++ src/components/Interests.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Interests = () => {
+  return (
+    <div className="interest-bg-container flex flex-col justify-center" id="interests">
+      <h1 className="interests-heading">INTERESTS</h1>
+
+      <p className="interest text-justify">
+        Apart from being a web developer, I enjoy most of my time being
+        outdoors. In the winter, I am an avid skiier and novice ice climber.
+        During the warmer months here in Colorado, I enjoy mountain biking, free
+        climbing, and kayaking.
+      </p>
+      <br />
+
+      <p className="interest text-justify">
+        When forced indoors, I follow a number of sci-fi and fantasy genre
+        movies and television shows, I am an aspiring chef, and I spend a large
+        amount of my free time exploring the latest technolgy advancements in
+        the front-end web development world.
+      </p>
+    </div>
+  );
+};
+
+export default Interests;
